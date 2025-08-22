dbb1eeb9ae5570c16b9f10142bc75b1364cbd5cb
divyam <divyam.iiitv@gmail.com>
2025-08-22 14:04:41 +0530

    Firebase initialized (messaging not working currently)

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,29 @@ import Register from "./components/Register.jsx";
 import Login from "./components/Login.jsx";
 import Home from "./components/Home.jsx";
 
+import { messaging } from "firebase";
+import { getToken } from "firebase/messaging";
+
+const VAPID_KEY = process.env.VAPID_KEY;
+
 function App() {
   const [isRegistered, setIsRegistered] = useState(null); // null = loading
+
+  async function requestPermission(){
+    const permission  = await Notification.requestPermission();
+
+    if(permission == "granted"){
+      //generate token
+      getToken(messaging, {vapidKey : VAPID_KEY});
+    }else if(permission == "denied"){
+      alert("You denied the notification permission");
+    }
+  }
+
+  useEffect(()=>{
+    //As soon as the App mounts we have to ask the user for their notification persmission
+    requestPermission();
+  })
 
   useEffect(() => {
     axios
---
+++ src/firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getMessaging } from "firebase/messaging";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBjifgEMmEzIMeVL9UzVG3mMxnjwC1_BEw",
+  authDomain: "contestify-3209e.firebaseapp.com",
+  projectId: "contestify-3209e",
+  storageBucket: "contestify-3209e.firebasestorage.app",
+  messagingSenderId: "98458256488",
+  appId: "1:98458256488:web:4d43580f22ec49cf02e7dd",
+  measurementId: "G-EVDH1ZQ1K5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const messaging = getMessaging(app);
+const analytics = getAnalytics(app);
